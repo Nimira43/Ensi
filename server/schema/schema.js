514681f3c6f84dd197bf248c79a1c7c62ec887f2
Nimira43 <n.rabone72@aol.com>
2026-05-26 04:29:08 +0100

    Experimenting with the schema.

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -4,7 +4,8 @@ const {
   GraphQLObjectType,
   GraphQLID,
   GraphQLString,
-  GraphQLSchema
+  GraphQLSchema,
+  GraphQLList
 } = require('graphql')
 
 const ClientType = new GraphQLObjectType({
@@ -17,9 +18,38 @@ const ClientType = new GraphQLObjectType({
   })
 })
 
+const ProjectType = new GraphQLObjectType({
+  name: 'Project',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    status: { type: GraphQLString }
+  })
+})
+
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
+    projects: {
+      type: new GraphQLList(ProjectType),
+      resolve(parent, args) {
+        return projects
+      }
+    },
+    project: {
+      type: ProjectType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return projects.find(project => project.id === args.id)
+      }
+    },
+    clients: {
+      type: new GraphQLList(ClientType),
+      resolve(parent, args) {
+        return clients
+      }
+    },
     client: {
       type: ClientType,
       args: { id: { type: GraphQLID } },
